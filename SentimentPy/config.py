60eb633ea43671acd78466b177d95948a55a458e
gbates22 <grayson.bates@gmail.com>
2018-06-26 12:07:32 -0400

    Sentiment Bot first

---
+++ SentimentPy/config.py
@@ -0,0 +1,5 @@
+consumer_key = 'mjxu5skic7grMWHMK47Yv0N0u'
+consumer_secret = 'NhOh27BUCZzTV2bGpvhw7L2xgJyLydNFUSKmmuuLthSBt0L8J0'
+
+access_token = '282715739-UcbyxThNTVeiA7nmZpwPW8FfAvIslqgPVIVlHPRE'
+access_token_secret = 'tuQHaFr1wP38sQDb2cf5reP5nt4Cle9CFk5udLHyDfywr'
